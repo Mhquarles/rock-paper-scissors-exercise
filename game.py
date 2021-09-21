489de36f84217d2df735a35aaf050e67b5539cdb
Mhquarles <mhq3@georgetown.edu>
2021-09-21 12:13:44 -0400

    Create game.py

---
+++ game.py
@@ -0,0 +1,44 @@
+#game.py
+import random
+
+print("Rock, Paper, Scissors, Shoot!")
+
+# PROMPT USER FOR INPUT
+#x = input("Choose 'rock' or 'paper' or 'scissors'"" )
+user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+print("User chose:")
+print(user_choice)
+
+#COMPUTER CHOICE AT RANDOM
+
+options = ["rock", "paper", "siccors"]
+
+computer_choice = random.choice(options)
+print("Computer chose:")
+print(computer_choice)
+
+# adapted from code shared by Diego in Slack
+if computer_choice == user_choice:
+    print ("NO WINNER,TIE! TRY AGAIN!")
+elif computer_choice == 'rock' and user_choice == 'scissors':
+    print ("COMPUTER WINS, rock beat scissors, try again dumbie!")
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    print ("COMPUTER WINS, scissors beat paper, try again dumbie!")
+elif computer_choice == 'paper' and user_choice =='rock':
+    print ("COMPUTER WINS, paper beat rock, try again dumbie!")
+elif computer_choice == 'scissors' and user_choice == 'rock':
+    print ("YOU WIN! Guess you’re smarter than a computer;)")
+elif computer_choice == 'paper' and user_choice == 'scissors':
+    print ("YOU WIN! Guess you’re smarter than a computer;)")
+elif computer_choice == 'rock' and user_choice == 'paper':
+    print ("YOU WIN! Guess you’re smarter than a computer;)")
+
+# adapted from code shared by Dominic in Slack    
+user_choice=input("Choose 'rock' or 'paper' or 'scissors': ")
+if user_choice in ["rock","paper","scissors"]:
+    print("user chose:") 
+    print(user_choice)
+else: 
+    print("You choice is not valid.  Reminder- all letters must be lowercase!")
+    print("Try again")
+    exit()    
